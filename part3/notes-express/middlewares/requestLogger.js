2b84980a54ba2bb8e0ad385cecafd32b7e9975d6
LUÍS CARLOS MENDES DE OLIVEIRA <8190370@estg.ipp.pt>
2025-01-03 23:57:27 +0000

    update requestLogger

--- part3/notes-express/middlewares/requestLogger.js
+++ part3/notes-express/middlewares/requestLogger.js
@@ -1,9 +1,13 @@
 const requestLogger = (req, res, next) => {
-  console.log("Method:", req.method);
-  console.log("Path:  ", req.path);
-  console.log("Body:  ", req.body);
-  console.log("---");
+  const now = new Date();
+  console.log('Timestamp:', now.toISOString());
+  console.log('Method:', req.method);
+  console.log('Path:  ', req.path);
+  console.log('Body:  ', req.body);
+  console.log('IP:    ', req.ip);
+  console.log('---');
   next();
 };
+
 
 module.exports = requestLogger;
