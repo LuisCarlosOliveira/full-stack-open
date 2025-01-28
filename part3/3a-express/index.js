75d8a74b121d47a9becad3c97dfed56b59f63c88
LUÍS CARLOS MENDES DE OLIVEIRA <8190370@estg.ipp.pt>
2025-01-28 23:04:50 +0000

    add cors to part3 example

--- part3/3a-express/index.js
+++ part3/3a-express/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const cors = require('cors'); 
 const app = express();
+
+app.use(cors());
+
+/*
+app.use(cors({
+  origin: 'http://localhost:5173' // only this origin
+}));
+*/
 
 app.use(express.json());
 
